3890e4eaa3ec493ed4509da4c21c9b8e60a4f69f
Kevin-Winter-Codes <Kevin.Winter.Privat@gmail.com>
2020-12-15 16:21:41 +0100

    Tag 2: C# 7.3 C# 8.0 C# 9.0

---
+++ CSharpAdvanced_2020_12_14/CSharp_80/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CSharp_80
+{
+
+    public class TestClass : IDisposable
+    {
+        public string Title { get; set; } = "Hallo";
+
+        public TestClass()
+        {
+
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+
+    public interface ITest
+    {
+        public void Hallo()
+        {
+            Console.WriteLine("Hallo Welt");
+        }
+
+        public virtual void GutenAbend()
+        {
+            Console.WriteLine("Hallo Welt");
+        }
+
+        public abstract void GutenMorgen();
+
+        public void Wiederschauen();
+        
+    }
+
+    public class TestWithInterface : ITest
+    {
+        public void Wiederschauen()
+        {
+            Console.WriteLine("Wiederschauen");
+        }
+
+        public void GutenMorgen()
+        {
+            throw new NotImplementedException();
+        }
+
+        // In einer Instanz von TestWithInterface fehlt die Methode Hallo. 
+        // Allerdings kann man Hallo (Interface-Default Methode) mithilfe folgenden Quellcode aufrufen 
+
+        public void TestMe()
+        {
+            ITest myInterface = (ITest)this;
+            myInterface.Hallo();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestWithInterface test1 = new TestWithInterface(); //Klasseninstanz 
+            test1.TestMe();
+
+            ITest test = new TestWithInterface();
+            test.Hallo(); //Hier wird die Hallo - Methode aus dem Interface aufgerufen
+
+            #region Index-Ranges
+            string[] stringArray = { "Franz", "fährt", "in", "einem", "verwahlosten", "Taxi", "durch", "Bayern" };
+
+            Console.WriteLine($"{stringArray[^0]}");
+            Console.ReadKey();
+            #endregion
+            #region yield - Beispiel
+            GebeZahlenAus();
+            #endregion
+            #region Using 
+            for (int i = 0; i < 10; i++)
+            {
+                using TestClass test2 = new TestClass();
+            } //Dispo
+
+            //Hier kann nicht mehr auf test.Title zugegriffen werden
+
+            #endregion
+
+
+
+            #region VerbatinString
+
+            string consolenAusgabe = "Hallo Regina \n ich hoffe der Kurs bereitet Dir Freude"; //Text mit Escapezeichen
+            string filePath1 = "C:\\Temp\\anyDirectory";
+            string filePath2 = @"C:\Temp\anyDirectory";
+            string stringMitVariable = $"{filePath1} {filePath2}";
+            #endregion
+        }
+
+        public static async IAsyncEnumerable<int> GeneriereZahlen()
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                await Task.Delay(100);
+                yield return i; 
+                // Gebe pro Schleifendurchlauf einen Wert nach dem Anderen heraus, bis die Schleife durchgelaufen ist. 
+                // yield return sagt nicht, dass man eine Methode verlässt, wie bei einem einfachen Retun
+            }
+                
+        }
+
+        public static async void GebeZahlenAus()
+        {
+            await foreach (var zahl in GeneriereZahlen())
+            {
+                Console.WriteLine(zahl);
+            }
+        }
+    }
+}
---
+++ CSharpAdvanced_2020_12_14/CSharp_90/Program.cs
@@ -0,0 +1,148 @@
+﻿using System;
+
+namespace CSharp_90
+{
+
+    public class MyClass
+    {
+        public string Titel { get; set; } //Auto Property
+        public DateTime Date { get; set; }
+       
+
+        public MyClass()
+        {
+            Titel = string.Empty;
+            Date = DateTime.Now;
+        }
+
+        public MyClass (string titel, DateTime date)
+        {
+            Titel = titel;
+            Date = date;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass myClass1 = new MyClass();
+            MyClass myClass2 = new MyClass("Hallo", DateTime.Now);
+
+            MyClass myClass3 = new(); // MyClass myClass1 = new MyClass();
+            MyClass myClass4 = new("ABC", DateTime.Now);
+
+
+
+            PersonClass personenClass1 = new PersonClass("Max", "Musterfrau");
+            PersonClass personenClass2 = new PersonClass("Max", "Musterfrau");
+
+            // Referenzen werden geprüft 
+            if (personenClass1 == personenClass2)
+            {
+                Console.WriteLine("gleich!");
+            }
+            else
+            {
+                Console.WriteLine("ungleich!");
+            }
+
+            if (personenClass1.Equals(personenClass2))
+            {
+
+            }
+            else
+            {
+                //lande bei der Prüfung mit Equals
+            }
+
+           
+            
+            Person myFirstRecord1 = new("Kevin","Winter");
+            Person myFirstRecord2 = new("Kevin", "Winter");
+
+            //Werteprüfung
+            if (myFirstRecord1 == myFirstRecord2)
+            {
+                Console.WriteLine("gleich!");
+            }
+            else
+            {
+                Console.WriteLine("ungleich!");
+            }
+
+            if (myFirstRecord1.Equals(myFirstRecord2))
+            {
+                //lande nach der PRüfung hier. Bei Equals in Record = Werteprüfung
+            }
+
+            Console.WriteLine($"##### HashCodes class vs. records");
+            Console.WriteLine($"HashCode von PersonenClass1: {personenClass1.GetHashCode()}");
+            Console.WriteLine($"HashCode von PersonenClass2: {personenClass2.GetHashCode()}");
+            Console.WriteLine($"HashCode von myFirstRecord1: {myFirstRecord1.GetHashCode()}");
+            Console.WriteLine($"HashCode von myFirstRecord2: {myFirstRecord2.GetHashCode()}");
+
+
+            Console.WriteLine($"##### ToString class vs. records");
+            Console.WriteLine($"Class.ToString() -> {personenClass1.ToString()}");
+            Console.WriteLine($"Record.ToString() -> {myFirstRecord1.ToString()}");
+
+
+
+            var blog1 = new Blog("C# 9", "{ State of .NET 5 } rockt!",
+                new[]
+                {
+                    "Hat spaß gemacht!",
+                    "Konnte vom HomeOffice teilnehmen :-)"
+                });
+
+            var blog2 = new Blog("C# 9", "{ State of .NET 5 } rockt!",
+                new[]
+                {
+                    "Hat spaß gemacht!",
+                    "Konnte vom HomeOffice teilnehmen :-)"
+                });
+
+            Console.WriteLine($"{blog1.ToString()}");
+
+            Console.ReadLine();
+
+
+
+        }
+    }
+
+
+
+
+    #region Records
+    public record Blog(string Title, string Content, string[] Comments);
+    public record Person(string Vorname, string Nachname);
+    public record Employee(string Firmennamen, string Vorname, string Nachname) : Person(Vorname, Nachname);
+    public record Haus(Person person);
+    public record PersonenListe(string Veranstaltung, string[] Namen);
+
+    public record Artikel
+    {
+        public string Titel { get; init; } //Artikel muss Konstruktor zugewiesen
+        public string Content { get; set; }
+    }
+    public class PersonClass
+    {
+        public string Vorname { get; init; }
+        public string Nachname { get; init; }
+
+        public PersonClass(string vorname, string nachname)
+        {
+            Vorname = vorname;
+            Nachname = nachname;
+        }
+
+        public void Deconstruct(out string vorname, out string nachname)
+        {
+            nachname = Nachname;
+            vorname = Vorname;
+        }
+    }
+
+    #endregion
+}
